73b3f7ba52d19991bd62e370940eadbcce5ee91c
gabrielphala <gabrielphala@outlook.com>
2023-05-02 23:23:17 +0200

    better support for different data types

--- src/Utility.js
+++ src/Utility.js
@@ -1,3 +1,5 @@
+const SQLDate = require("./Date")
+
 module.exports = class Utility {
     static removeAtEndOfString (str, toRemove) {
         str = str.trim()
@@ -5,11 +7,22 @@ module.exports = class Utility {
         return str.substring(0, str.length - toRemove.length)
     }
 
+    static isDate (obj) {
+        if (Object.prototype.toString.call(obj) === '[object Date]') return true;
+        else return false;
+    }
+
     static normalizeValue (value, isRaw = false) {
-        if (value == null || typeof value == 'boolean' || typeof value == 'number')
+        if (value == null || typeof value == 'number')
             return value;
 
+        else if (typeof value == 'boolean') return value ? 1 : 0;
+
         else if (typeof value == 'string') return `${isRaw ? value : `'${value}'`}`;
+
+        else if (typeof value == 'function') return this.normalizeValue(value(), isRaw)
+
+        else if (Utility.isDate(value)) return `${isRaw ? SQLDate.toSQLDatetime(value) : `'${SQLDate.toSQLDatetime(value)}'`}`;
 
         else if (typeof value == 'object') {
             switch (Object.keys(value)[0]) {
@@ -24,7 +37,12 @@ module.exports = class Utility {
     }
 
     static evalOperators (obj) {
-        if (obj == null || typeof obj != 'object') return '=';
+        if (
+            obj == null ||
+            typeof obj != 'object' ||
+            Array.isArray(obj) ||
+            Utility.isDate(obj)
+        ) return '=';
 
         const ops = {
             '$ne': '!=',
@@ -45,6 +63,16 @@ module.exports = class Utility {
 
             cb(key, data[key])
         }
+    }
+
+    static extractSchemaColumns (schema) {
+        const arr = [];
+
+        Utility.each(schema, columnName => {
+            arr.push(columnName)
+        })
+
+        return arr;
     }
 
     static exitOnError (err) {
